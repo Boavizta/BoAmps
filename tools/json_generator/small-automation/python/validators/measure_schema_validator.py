5e7568a2749b36393726e769824146221777e002
EstebanCatte <80264289+EstebanCatte@users.noreply.github.com>
2024-11-15 16:09:03 +0100

    make argument optinal + type debug

--- tools/json_generator/small-automation/python/validators/measure_schema_validator.py
+++ tools/json_generator/small-automation/python/validators/measure_schema_validator.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
+from typing import Optional
 
 class Measure(BaseModel):
-    measurementMethod: str
-    manufacturer: str
-    version : str
-    cpuTrackingMode : str
-    gpuTrackingMode : str
+    measurementMethod: Optional[str] = None
+    manufacturer: Optional[str] = None
+    version : Optional[str] = None
+    cpuTrackingMode : Optional[str] = None
+    gpuTrackingMode : Optional[str] = None
     averageUtilizationCpu : Annotated[float, Field(ge=0.0, le=1.0)]
     averageUtilizationGpu : Annotated[float, Field(ge=0.0, le=1.0)]
-    serverSideInference : Annotated[str, Field(regex=r"(modelServer|inferenceServer|both)")]
-    unit : Annotated[str, Field(regex=r"(mWh|Wh|kWh|MWh|GWh|kJoule|MJoule|GJoule|TJoule|PJoule|BTU|kiloFLOPS|megaFLOPS|gigaFLOPS|teraFLOPS|petaFLOPS|exaFLOPS|zettaFLOPS|yottaFLOPS")]
-    powerCalibrationMeasurement : float
-    durationCalibrationMeasurement : float
-    powerConsumption : float
-    measurementDuration : float
-    measurementDateTime : int
-    share: Annotated[float, Field(ge=0.0, le=1.0)]
+    serverSideInference : Annotated[str, Field(pattern=r"(modelServer|inferenceServer|both)")]
+    unit : Annotated[str, Field(pattern=r"(mWh|Wh|kWh|MWh|GWh|kJoule|MJoule|GJoule|TJoule|PJoule|BTU|kiloFLOPS|megaFLOPS|gigaFLOPS|teraFLOPS|petaFLOPS|exaFLOPS|zettaFLOPS|yottaFLOPS)")]
+    powerCalibrationMeasurement : Optional[float] = 0.0
+    durationCalibrationMeasurement : Optional[float] = 0.0
+    powerConsumption : Optional[float] = 0.0
+    measurementDuration : Optional[float] = 0.0
+    measurementDateTime : Optional[int] = 0
+    share: Optional[Annotated[float, Field(ge=0.0, le=1.0)]] = 0.0
