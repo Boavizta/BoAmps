9bbdd46affc0bcfe64540bd5fb92d78359efdae2
romain.carbou@orange.com <romain.carbou@orange.com>
2024-11-28 23:52:18 +0100

    unit tests for flattener raising possible ISSUE regarding the format

--- tools/common/flattener_test.py
+++ tools/common/flattener_test.py
@@ -1,10 +1,12 @@
 
 import unittest
 from .flattener import BoAmpsFlattener
+import os
+import json
 
 class BoAmpsFlattenerTest(unittest.TestCase):
 
-    def testGetSpace(self):
+    def testGetRaw(self):
         """Tests flattener.
         """
         obj = { 
@@ -19,7 +21,69 @@ class BoAmpsFlattenerTest(unittest.TestCase):
             } 
         }
         flattener = BoAmpsFlattener(obj=obj)
-        out = flattener.get_raw()
-        expected = [ {'foo': 10, 'bar': 20, 'nested.foo.0': 3, 'nested.foo.1': 2, 'nested.foo.2': 1, 'nested.bar.0.0.x': True, 'nested.bar.0.1.y': False} ]
+        out = flattener.get_raw().to_dict(orient='records')
+        expected = [{
+            'foo': 10, 
+            'bar': 20, 
+            'nested.foo.0': 3, 
+            'nested.foo.1': 2, 
+            'nested.foo.2': 1, 
+            'nested.bar.0.0.x': True, 
+            'nested.bar.0.1.y': False
+        }]
         print(str(out))
         self.assertEqual(out == expected, True)
+
+
+    def testGetDenumeratedAmbiguous(self):
+        """Tests flattener with denumerated columns.
+        """
+        obj = { 
+            'foo': 10, 
+            'bar': 20, 
+            'nested': { 
+                'foo': [ 3, 2, 1 ], 
+                'bar': [ [ 
+                    { 'x': True }, 
+                    { 'y': False } 
+                ] ]
+            } 
+        }
+        flattener = BoAmpsFlattener(obj=obj)
+        out = flattener.get_denumerated().to_dict(orient='records')
+        expected = []
+        print(str(out))
+        self.assertEqual(out == expected, True)
+
+    def testGetDenumeratedNonAmbiguous(self):
+        """Tests flattener with denumerated columns.
+        """
+        obj = { 
+            'foo': 10, 
+            'bar': 20, 
+            'nested': { 
+                'bar': [ [ 
+                    { 'x': True }, 
+                    { 'y': False } 
+                ] ]
+            } 
+        }
+        flattener = BoAmpsFlattener(obj=obj)
+        out = flattener.get_denumerated().to_dict(orient='records')
+        expected = [{
+            'foo': 10, 
+            'bar': 20, 
+            'nested.bar.x': True, 
+            'nested.bar.y': False
+        }]
+        print(str(out))
+        self.assertEqual(out == expected, True)
+
+    def testGetDenumeratedAmbiguousLong(self):
+        """Tests flattener with denumerated columns.
+        """
+        with open(os.sep.join([ '..', 'examples', 'energy-report-example-1.json'])) as f:            
+            obj = json.load(f)
+            flattener = BoAmpsFlattener(obj=obj)
+            out = flattener.get_denumerated().to_dict(orient='records')
+            self.assertEqual(len(out) > 0, True)
